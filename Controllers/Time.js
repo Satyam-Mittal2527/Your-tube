e55321010b292c2a40dbc47e0b8cc5d564494058
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2025-05-17 18:04:52 +0545

    Update File

--- Controllers/Time.js
+++ Controllers/Time.js
@@ -1,10 +1,17 @@
+import axios from "axios"
+
 export const TimeController = async(req,res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    // const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const ip= req.ip;
     try {
+        const response = await axios.get(`http://ip-api.com/json/${ip}`)
+        // console.log(response)
+        const resCity = response.data.regionName;
+        // console.log(resCity)
         const date = new Date();
         const time= date.getHours()
         // console.log(time)
-        res.status(200).json({hour:time});
+        res.status(200).json({hour:time , city : resCity});
     } catch (error) {
         res.status(400).json(error)
     }
