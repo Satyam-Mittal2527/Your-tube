4559fc6e97543310671dcc8fdae6417b85f6c3bd
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2025-05-20 15:30:29 +0545

    Update files

--- Controllers/SmsOtp.js
+++ Controllers/SmsOtp.js
@@ -10,16 +10,17 @@ export const smsOtp = async (req, res) => {
   const client = twilio(accountSid, authToken);
   const otp = Math.floor(Math.random() * 100000);
   try {
+    console.log("Sending sms");
     const message= await client.messages
       .create({
         body: `${otp}`,
         from: "+19207893050",
         to: `${phone}`,
       });
-
+      console.log("Sent Otp");
       console.log(message.sid);
-      return res.status(200).json({ message: "Otp sent successfully", otp : otp});
+      res.status(200).json({ message: "Otp sent successfully", otp : otp});
   } catch (error) {
-    return res.status(400).json("Error sms", error);
+    res.status(400).json("Error sms", error);
   }
 };
