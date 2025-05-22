d6d6242984bd6e328802258fc5dac12be85f6999
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2025-05-22 12:12:40 +0545

    Update files

--- Controllers/SmsOtp.js
+++ Controllers/SmsOtp.js
@@ -1,47 +1,47 @@
-import axios from "axios"
+import axios from "axios";
 
 export const smsOtp = async (req, res) => {
-  console.log("In the controller")  
-  const {phone} = req.body;
-    console.log(phone)
-    try {
-       const response = await axios.get(
-              "https://www.fast2sms.com/dev/bulkV2",
-              {
-                params: {
-                  authorization:
-                    "x7dOL9i5MSmeKibU4AlFC9flXaBKKAdnrvHPgYNybGylmC6ZQD0pHdtqVeh3",
-                  variables_values: "5599",
-                  route: "otp",
-                  numbers: phone,
-                },
-                headers: {
-                  "cache-control": "no-cache",
-                },
-              }
-            );
-
-            console.log("SMS Response:", response.data);
-    } catch (error) {
-      console.log("smsotyp:",error)
-    }
-  // const accountSid =process.env.accountSid;
-  // console.log(accountSid)
-  // const authToken = process.env.authToken;
-  // console.log(authToken)
-  // const client = twilio(accountSid, authToken);
-  // const otp = Math.floor(Math.random() * 100000);
+  console.log("In the controller");
+  const { phone } = req.body;
+  console.log(phone);
+  const accountSid = process.env.accountSid;
+  console.log(accountSid);
+  const authToken = process.env.authToken;
+  console.log(authToken);
+  const client = twilio(accountSid, authToken);
+  const otp = Math.floor(Math.random() * 100000);
+  try {
+    client.messages
+      .create({
+        body: `${otp}`,
+        from: "+19207893050",
+        to: `${phone}`,
+      })
+      .then((message) =>
+        res.status(200).json({ message: "Otp sent successfully", otp: otp })
+      );
+  } catch (error) {
+    res.json(400).json("Error sms", error);
+  }
   // try {
-  //   client.messages
-  //     .create({
-  //       body: `${otp}`,
-  //       from: "+19207893050",
-  //       to: `${phone}`,
-  //     })
-  //     .then((message) => res
-  //     .status(200)
-  //     .json({ message: "Otp sent successfully", otp : otp}));
+  //    const response = await axios.get(
+  //           "https://www.fast2sms.com/dev/bulkV2",
+  //           {
+  //             params: {
+  //               authorization:
+  //                 "x7dOL9i5MSmeKibU4AlFC9flXaBKKAdnrvHPgYNybGylmC6ZQD0pHdtqVeh3",
+  //               variables_values: "5599",
+  //               route: "otp",
+  //               numbers: phone,
+  //             },
+  //             headers: {
+  //               "cache-control": "no-cache",
+  //             },
+  //           }
+  //         );
+
+  //         console.log("SMS Response:", response.data);
   // } catch (error) {
-  //   res.json(400).json("Error sms", error);
+  //   console.log("smsotyp:",error)
   // }
 };
