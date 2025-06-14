34fcab54124c7a37b34ecae3c7fd1ddb03f360ca
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2025-06-14 11:45:16 +0545

    Update Files

--- Controllers/OtpGenerator.js
+++ Controllers/OtpGenerator.js
@@ -4,28 +4,21 @@ export const Otp = async (req, res) => {
   const { email } = req.body;
   const {phone} = req.body
   const cities = ["Tamil Nadu", "Kerala", "karnataka", "Andhra", "Telungana"];
-  let condition = false;
+  // let condition = false;
   const ip = req.ip;
   try {
     const response = await axios.get(`http://ip-api.com/json`);
     console.log(response);
-    if (cities.map(c => c.toLowerCase()).includes(response.data.regionName.toLowerCase())) {
-      condition = true;
-    }
+    // if (cities.map(c => c.toLowerCase()).includes(response.data.regionName.toLowerCase())) {
+    //   condition = true;
+    // }
     let responseOtp = 0;
-    if (condition) {
-      responseOtp = await axios.post("https://your-tube-ovhq.onrender.com/user/TriggerMail", {
+    responseOtp = await axios.post("https://your-tube-ovhq.onrender.com/user/TriggerMail", {
         email,
-      });
-      console.log("Returned Otp Code:", responseOtp.data.OtpCode);
-      condition = true;
-      res.status(200).json({OtpCode : responseOtp.data.OtpCode})
-    }else{
-        // console.log("Calling sms")
-        responseOtp = await axios.post("https://your-tube-ovhq.onrender.com/user/SmsOtp",{phone: phone})
-        // console.log("responseOtp:",responseOtp.data.otp)
-        res.status(200).json({OtpCode : responseOtp.data.otp})
-    }
+    });
+    console.log("Returned Otp Code:", responseOtp.data.OtpCode);
+    // condition = true;
+    res.status(200).json({OtpCode : responseOtp.data.OtpCode})
     
   } catch (error) {
     console.log("again Otp error:",error)
